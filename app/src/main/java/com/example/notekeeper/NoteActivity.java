ebbdb17eb9c38aea0d8c7aeeeb21b675cd5365db
Ziyad Tarek <ziyad.tarek.ms@alexu.edu.eg>
2020-04-25 02:28:57 +0200

    added some functionality

--- app/src/main/java/com/example/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/notekeeper/NoteActivity.java
@@ -1,5 +1,6 @@
 package com.example.notekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,11 +13,15 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
+    public static final String NOTE_INFO = "com.example.notekeeper.NOTE_INFO";
+    private NoteInfo mNote;
+    private boolean mIsNewNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +36,28 @@ public class NoteActivity extends AppCompatActivity {
         ArrayAdapter<CourseInfo> adapterCourses =
                 new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         spinnerCourses.setAdapter(adapterCourses);
+
+        readDisplayStateValue();
+        EditText textNoteTitle = findViewById(R.id.text_note_title);
+        EditText textNoteText = findViewById(R.id.text_note_text);
+
+        if(!mIsNewNote)
+            DisplayNote(spinnerCourses, textNoteTitle, textNoteText);
+    }
+
+    private void DisplayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        int courseIndex = courses.indexOf(mNote.getCourse());
+        spinnerCourses.setSelection(courseIndex);
+        textNoteTitle.setText(mNote.getTitle());
+        textNoteText.setText(mNote.getText());
+    }
+
+    private void readDisplayStateValue() {
+        Intent intent = getIntent();
+        mNote = intent.getParcelableExtra(NOTE_INFO);
+        mIsNewNote = mNote == null;
     }
 
     @Override
--- app/src/main/java/com/example/notekeeper/NoteInfo.java
+++ app/src/main/java/com/example/notekeeper/NoteInfo.java
@@ -3,7 +3,7 @@ package com.example.notekeeper;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public final class NoteInfo {
+public final class NoteInfo implements Parcelable{
     private CourseInfo mCourse;
     private String mTitle;
     private String mText;
@@ -12,6 +12,12 @@ public final class NoteInfo {
         mCourse = course;
         mTitle = title;
         mText = text;
+    }
+
+    private NoteInfo(Parcel parcel) {
+        mCourse = parcel.readParcelable(CourseInfo.class.getClassLoader());
+        mTitle = parcel.readString();
+        mText = parcel.readString();
     }
 
     public CourseInfo getCourse() {
@@ -62,4 +68,26 @@ public final class NoteInfo {
         return getCompareKey();
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(mCourse,0);
+        dest.writeString(mTitle);
+        dest.writeString(mText);
+    }
+    public static final Parcelable.Creator<NoteInfo> CREATOR = new Parcelable.Creator<NoteInfo>(){
+        @Override
+        public NoteInfo createFromParcel(Parcel source) {
+            return new NoteInfo(source);
+        }
+
+        @Override
+        public NoteInfo[] newArray(int size) {
+            return new NoteInfo[size];
+        }
+    };
 }
